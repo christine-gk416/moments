c22de39901ede452eb543534a56c41b979f44a8a
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-07-06 09:24:43 +0000

    Create basic signup form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import { Container} from "react-bootstrap";
-import { Route, Switch } from "react-router-dom"
+import { Route, Switch } from "react-router-dom";
+import './api/axiosDefault';
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -11,10 +13,9 @@ function App() {
         <Switch>
           <Route exact path="/" render={()=><h1>Homepage</h1>}/>
           <Route exact path="/signin" render={()=><h1>Sign in</h1>}/>
-          <Route exact path="/signup" render={()=><h1>Sign up</h1>}/>
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route render={() => <p>Page not found!</p>} />
         </Switch>
-        
-        
       </Container>
     </div>
   );
